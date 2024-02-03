0929e63ea85421188696e2135bb3fe55d1edfcb1
Md Sohail <talk2sohail100@gmail.com>
2024-02-03 23:25:19 +0530

    refactor to use async/await for handling promises

--- db.js
+++ db.js
@@ -9,8 +9,24 @@ client.setEndpoint(DB_URL).setProject(PROJECT_ID).setKey(API_KEY);
 
 const databases = new sdk.Databases(client);
 
-const jobs_collection = databases.listDocuments(DATABASE_ID, JOB_COLLECTION_ID);
-
 module.exports = {
-	jobs_collection,
+	get_job_data: async () => {
+		const jobs_collection = await databases.listDocuments(
+			DATABASE_ID,
+			JOB_COLLECTION_ID
+		);
+
+		if (jobs_collection) {
+			const data = jobs_collection.documents.map((job) => {
+				return {
+					role: job.job_name,
+					status: job.active ? "active" : "inactive",
+				};
+			});
+
+			return { ok: true, data: data };
+		}
+
+		return { ok: false, data: null };
+	},
 };
--- routes.js
+++ routes.js
@@ -1,31 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const { jobs_collection } = require("./db.js");
+const { get_job_data } = require("./db.js");
 
-router.get("/jobs", function (req, res) {
-	jobs_collection
-		.then(function (c) {
-			if (c) {
-				const data = c.documents.map(function (job) {
-					return {
-						role: job.job_name,
-						status: job.active ? "active" : "inactive",
-					};
-				});
-				res.status(200).send({
-					count: c.total,
-					data: data,
-				});
-			}
-		})
-		.catch(function (e) {
-			console.log("Collection fetch failed, error: ", e);
-			res.status(404).send({
-				message: "NO DATA",
-				success: "false",
-			});
+router.get("/jobs", async function (req, res) {
+	const response = await get_job_data();
+	if (response.ok) {
+		res.status(200).send({
+			data: response,
 		});
+		return;
+	}
+
+	res.status(404).send({
+		message: "no data",
+	});
 });
 
 module.exports = router;
